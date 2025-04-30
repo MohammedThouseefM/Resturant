ed3e1f6b715e8f3ac56b012b0bc767c57cf9573b
MohammedThouseefM <mthouseef100@gmail.com>
2025-04-30 11:58:33 +0530

    first commit

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    let navbar = document.getElementById('navbar');
+    if (window.scrollY > 100) {
+      navbar.classList.add('navcolor');
+    } else {
+      navbar.classList.remove('navcolor');
+    }
+  });
+  
